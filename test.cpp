bbba1d8428fef8069c858d59db7dfa4a41de604c
Luke2336 <55865338+Luke2336@users.noreply.github.com>
2020-03-30 15:37:13 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,13 +1,12 @@
-for (int i = 0; i < n - 1; i++) {
-    bool flag = false;
-    for (int j = 0; j < n - i - 1; j++) {
-        if (a[j] > a[j + 1]) {
-            // Swap(a[j], a[j + 1]);
-            int tmp = a[j];
-            a[j] = a[j + 1];
-            a[j + 1] = tmp;
-            flag = true;
+void Swap (int &a, int &b) {
+    int tmp = a; a = b; b = tmp;
+}
+void SelectionSort (vector<int> &a) {
+    int n = a.size();
+    for (int i = 1; i < n; i++) {
+        for (int j = i - 1; j >= 0; j--) {
+            if (a[j] > a[j + 1]) Swap(a[j], a[j + 1]);
+            else break;
         }
     }
-    if (!flag) break; // Sorted already
 }

d8c2d767f39515b7a5aa1a66a0b5390ca232a087
Luke2336 <55865338+Luke2336@users.noreply.github.com>
2020-03-30 15:14:18 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,22 +1,13 @@
-#include<bits/stdc++.h>
-using namespace std;
-void BubbleSort (vector<int> &a) {
-    int n = a.size();
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (a[j] > a[j + 1]) {
-                // Swap(a[j], a[j + 1]);
-                int tmp = a[j];
-                a[j] = a[j + 1];
-                a[j + 1] = tmp;
-            }
+for (int i = 0; i < n - 1; i++) {
+    bool flag = false;
+    for (int j = 0; j < n - i - 1; j++) {
+        if (a[j] > a[j + 1]) {
+            // Swap(a[j], a[j + 1]);
+            int tmp = a[j];
+            a[j] = a[j + 1];
+            a[j + 1] = tmp;
+            flag = true;
         }
     }
-}
-int main() {
-    vector<int> a = {6, 7, 4, 2, 1};
-    BubbleSort(a);
-    for (int x : a)
-        cout << x << " ";
-    return 0;
+    if (!flag) break; // Sorted already
 }

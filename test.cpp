0494493aa4a5eec6d57919d99387bc0de26e06cb
Luke2336 <55865338+Luke2336@users.noreply.github.com>
2020-04-27 17:47:26 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,17 +1,6 @@
-#define SIZE 10000
-int st[SIZE];
-int sz = 0;
-bool empty() {
-    return sz <= 0;
-}
-void push(int x) {
-    st[sz] = x;
-    sz++;
-}
-void pop() {
-    if (!empty()) sz--;
-}
-int top() {
-    if (!empty()) return st[sz - 1];
-    return -1;
-}
+#include <stack>
+stack<int> st;
+st.push(1);
+st.pop();
+int a = st.top();
+bool empty = st.empty();

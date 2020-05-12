44eecb336c8b6249f9ff9179b6d648849a43a5ba
Luke2336 <55865338+Luke2336@users.noreply.github.com>
2020-05-12 21:07:56 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,19 +1,48 @@
-int main() {
-    int G[5][5] = {
-        {0, 1 ,0, 1, 1},
-        {0, 1, 1, 0, 0},
-        {0, 1, 0, 1, 0},
-        {1, 0, 1, 0, 0},
-        {1, 0, 0, 0, 0}
-    };
-    bool visit[5] = {0};
-    stack<int> st;
-    st.push(0), visit[0] = true;
-    while (!st.empty()) {
-        int u = st.pop();
-        for (int i = 0; i < 5; i++)
-            if (G[u][i] && !visit[i])
-                st.push(i), visit[i] = true;
+#include <bits/stdc++.h>
+using namespace std;
+struct Node {
+    int val;
+    Node *left = NULL;
+    Node *right = NULL;
+    Node () {}
+    Node (int _val) { val = _val;}
+};
+struct BST {
+    Node *root = NULL;
+    bool find(int key) {
+        Node *tmp = root;
+        while (tmp && tmp->val != key) {
+            if (tmp->val > key) tmp = tmp->left;
+            else tmp = tmp->right;
+        }
+        return tmp != NULL;
     }
+    void add(int key) {
+        if (find(key)) return; //key已經在樹裡
+        Node *new_node = new Node(key);
+        Node *par, *tmp = root;
+        if (!root) { //樹是空的
+            root = new_node;
+            return;
+        }
+        while (tmp) { //當tmp為NULL時，代表找到空位插入new_node
+            par = tmp; //紀錄tmp當前的par
+            if (tmp->val > key) tmp = tmp->left;
+            else tmp = tmp->right;
+        }
+        //檢查要放par的左/右
+        if (par->val > key) par->left = new_node;
+        else par->right = new_node;
+    }
+};
+int main() {
+    BST bst;
+    bst.add(1);
+    cout << (bst.find(1) ? "Find\n" : "Not find\n");
+    cout << (bst.find(2) ? "Find\n" : "Not find\n");
+    bst.add(3);
+    bst.add(2);
+    cout << (bst.find(2) ? "Find\n" : "Not find\n");
+    cout << (bst.find(3) ? "Find\n" : "Not find\n");
     return 0;
 }

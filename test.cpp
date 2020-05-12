efe9e8bd3d7771ad8652256da644ce884150bd21
Luke2336 <55865338+Luke2336@users.noreply.github.com>
2020-05-12 15:24:30 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,18 +1,19 @@
-int G[5][5] = {
-    {0, 1 ,0, 1, 1},
-    {0, 1, 1, 0, 0},
-    {0, 1, 0, 1, 0},
-    {1, 0, 1, 0, 0},
-    {1, 0, 0, 0, 0}
-};
-bool visit[5] = {0};
-void dfs(int u) {
-    if (visit[u]) return;
-    cout << u << " ";
-    visit[u] = true;
-    for (int i = 0; i < 5; i++) dfs(i);
-}
 int main() {
-    dfs(0);
+    int G[5][5] = {
+        {0, 1 ,0, 1, 1},
+        {0, 1, 1, 0, 0},
+        {0, 1, 0, 1, 0},
+        {1, 0, 1, 0, 0},
+        {1, 0, 0, 0, 0}
+    };
+    bool visit[5] = {0};
+    stack<int> st;
+    st.push(0), visit[0] = true;
+    while (!st.empty()) {
+        int u = st.pop();
+        for (int i = 0; i < 5; i++)
+            if (G[u][i] && !visit[i])
+                st.push(i), visit[i] = true;
+    }
     return 0;
 }

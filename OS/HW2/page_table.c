acb870eeefc43551536e3150e1b5c9dfb3745ec5
Luke2336 <lukeyu.eecs07@nctu.edu.tw>
2021-01-07 10:52:51 +0800

    update page_table.x

--- OS/HW2/page_table.c
+++ OS/HW2/page_table.c
@@ -30,6 +30,7 @@ uint64_t read_physical_address(uint64_t physical_address)
 	return cmd.ret;
 }
 
+
 // Write value to a physical address
 void write_physical_address(uint64_t physical_address, uint64_t value)
 {
@@ -40,6 +41,31 @@ void write_physical_address(uint64_t physical_address, uint64_t value)
 	cmd.request[2] = value;
 	ret = ioctl(fd, IOCTL_REQUEST, &cmd);
 }
+
+// my functions------------------------------------------
+uint64_t get_entry(uint64_t x, int shift) {
+	return (x >> shift) & 0x1ffllu;
+}
+uint64_t get_addr(uint64_t addr, uint64_t id) {
+	return (addr & 0xffffffffff000llu) | (id << 3);
+}
+uint64_t get_pt_addr(uint64_t vir_addr)
+{
+	uint64_t cr3 = get_cr3_value();
+	uint64_t id_pml4 = get_entry(vir_addr, 39);
+	uint64_t id_pdpt = get_entry(vir_addr, 30);
+	uint64_t id_pd = get_entry(vir_addr, 21);
+	uint64_t id_pt = get_entry(vir_addr, 12);
+	uint64_t addr_pml4 = get_addr(cr3, id_pml4);
+	uint64_t cont_pml4 = read_physical_address(addr_pml4);
+	uint64_t addr_pdpt = get_addr(cont_pml4, id_pdpt);
+	uint64_t cont_pdpt = read_physical_address(addr_pdpt);
+	uint64_t addr_pd = get_addr(cont_pdpt, id_pd);
+	uint64_t cont_pd = read_physical_address(addr_pd);
+	uint64_t addr_pt = get_addr(cont_pd, id_pt);
+	return addr_pt;
+}
+// -------------------------------------------------------
 
 int main()
 {
@@ -63,8 +89,12 @@ int main()
 	// ------------------------------------------------
 	// Modify page table entry of y
 	// Let y point to x's physical address
-	uint64_t cr3 = get_cr3_value();
-	// ------------------------------------------------
+	uint64_t addr_x = get_pt_addr((uint64_t)x);
+	uint64_t addr_y = get_pt_addr((uint64_t)y);
+	uint64_t cont_x = read_physical_address(addr_x);
+	uint64_t cont_y = read_physical_address(addr_y);
+	write_physical_address(addr_y, cont_x);
+	//------------------------------------------------
 
 	getchar();
 
@@ -84,6 +114,7 @@ int main()
 	// Recover page table entry of y
 	// Let y point to its original address
 	// You may need to store y's original address at previous step
+	write_physical_address(addr_y, cont_y);
 	// ------------------------------------------------
 
 	getchar();
